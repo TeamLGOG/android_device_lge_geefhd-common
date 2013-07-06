206788f7229728f4cf13afc6adfbf4c0f73185ae
Tyler Montgomery <tyler@2440038.pubip.peer1.net>
2013-07-06 03:18:34 +0000

    fixups for cm10.1, oops!

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,8 +18,8 @@
 BOARD_KERNEL_CMDLINE := vmalloc=600M console=ttyHSL0,115200,n8 lpj=67677 user_debug=31 msm_rtb.filter=0x0 ehci-hcd.park=3 coresight-etm.boot_enable=0 androidboot.hardware=gkatt uart_console=disable lge.rev=rev_10 lcd_maker_id=primary lge.hreset=off, lge.reset=rst_etc gpt=enable logoentry=0x77665501+0+0 lge.batt_info=ds2704_l lge.crash=0x0 lge.crash_enable=0x0
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
-#BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
-BOARD_FORCE_RAMDISK_ADDRESS := 0x82200000
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
+#BOARD_FORCE_RAMDISK_ADDRESS := 0x82200000
 
 TARGET_KERNEL_CONFIG := gkatt_bcm-perf_defconfig
 TARGET_KERNEL_SOURCE := kernel/lge/geefhd
