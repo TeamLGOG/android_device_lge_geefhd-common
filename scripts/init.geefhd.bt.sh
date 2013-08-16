8b1f64e462aa322c730016a98bebe64248049968
madmack <alali48@gmail.com>
2013-08-16 10:43:30 -0400

    init.geefhd.bt.sh fixups and change btnvtool to our own bdAddrLoader
    this gets loaded once at boot time via an init service "config_bluetooth"
    btnvtool uses the /persist partition to load the BT MAC address which we lack.
    Our mac address is located in the /misc partition so this utility is useless.
    
    Change-Id: I03596a5190c9d20ab21bdc3a43e2d8fb130996b2

--- scripts/init.geefhd.bt.sh
+++ scripts/init.geefhd.bt.sh
@@ -2,38 +2,26 @@
 # Copyright (c) 2009-2012, Code Aurora Forum. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are 
-met:
+# modification, are permitted provided that the following conditions are met:
 #     * Redistributions of source code must retain the above copyright
 #       notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above 
-copyright
-#       notice, this list of conditions and the following disclaimer in 
-the
-#       documentation and/or other materials provided with the 
-distribution.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
 #     * Neither the name of Code Aurora nor
 #       the names of its contributors may be used to endorse or promote
-#       products derived from this software without specific prior 
-written
+#       products derived from this software without specific prior written
 #       permission.
 #
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-"AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
-THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
-PURPOSE AND
-# NON-INFRINGEMENT ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT 
-OWNER OR
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NON-INFRINGEMENT ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
 # CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
 # EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
-PROFITS;
-# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
-LIABILITY,
-# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
-OR
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
@@ -65,7 +53,7 @@ failed ()
 
 program_bdaddr ()
 {
-  /system/bin/btnvtool -O
+  /system/bin/bdAddrLoader
   logi "Bluetooth Address programmed successfully"
 }
 
@@ -188,8 +176,7 @@ case "$config" in
 esac
 
 # mimic hciattach options parsing -- maybe a waste of effort
-USAGE="hciattach [-n] [-p] [-b] [-t timeout] [-s initial_speed] <tty> 
-<type | id> [speed] [flow|noflow] [bdaddr]"
+USAGE="hciattach [-n] [-p] [-b] [-t timeout] [-s initial_speed] <tty> <type | id> [speed] [flow|noflow] [bdaddr]"
 
 while getopts "blnpt:s:" f
 do
@@ -202,8 +189,7 @@ do
 done
 shift $(($OPTIND-1))
 
-# Note that "hci_qcomm_init -e" prints expressions to set the shell 
-variables
+# Note that "hci_qcomm_init -e" prints expressions to set the shell variables
 # BTS_DEVICE, BTS_TYPE, BTS_BAUD, and BTS_ADDRESS.
 
 #Selectively Disable sleep
@@ -226,10 +212,8 @@ case $POWER_CLASS in
   3) PWR_CLASS="-p 2" ;
      logi "Power Class: CUSTOM";;
   *) PWR_CLASS="";
-     logi "Power Class: Ignored. Default(1) used 
-(1-CLASS1/2-CLASS2/3-CUSTOM)";
-     logi "Power Class: To override, Before turning BT ON; setprop 
-qcom.bt.dev_power_class <1 or 2 or 3>";;
+     logi "Power Class: Ignored. Default(1) used (1-CLASS1/2-CLASS2/3-CUSTOM)";
+     logi "Power Class: To override, Before turning BT ON; setprop qcom.bt.dev_power_class <1 or 2 or 3>";;
 esac
 
 case $LE_POWER_CLASS in
@@ -240,22 +224,15 @@ case $LE_POWER_CLASS in
   3) LE_PWR_CLASS="-P 2" ;
      logi "LE Power Class: CUSTOM";;
   *) LE_PWR_CLASS="-P 1";
-     logi "LE Power Class: Ignored. Default(2) used 
-(1-CLASS1/2-CLASS2/3-CUSTOM)";
-     logi "LE Power Class: To override, Before turning BT ON; setprop 
-qcom.bt.le_dev_pwr_class <1 or 2 or 3>";;
+     logi "LE Power Class: Ignored. Default(2) used (1-CLASS1/2-CLASS2/3-CUSTOM)";
+     logi "LE Power Class: To override, Before turning BT ON; setprop qcom.bt.le_dev_pwr_class <1 or 2 or 3>";;
 esac
 
-#echo 0 > /sys/module/hci_smd/parameters/hcismd_set
-
-eval $(/system/bin/hci_qcomm_init -e $PWR_CLASS $LE_PWR_CLASS && echo 
-"exit_code_hci_qcomm_init=0" || echo "exit_code_hci_qcomm_init=1")
+eval $(/system/bin/hci_qcomm_init -e $PWR_CLASS $LE_PWR_CLASS && echo "exit_code_hci_qcomm_init=0" || echo "exit_code_hci_qcomm_init=1")
 
 case $exit_code_hci_qcomm_init in
-  0) logi "Bluetooth QSoC firmware download succeeded, $BTS_DEVICE 
-$BTS_TYPE $BTS_BAUD $BTS_ADDRESS";;
-  *) failed "Bluetooth QSoC firmware download failed" 
-$exit_code_hci_qcomm_init;;
+  0) logi "Bluetooth QSoC firmware download succeeded, $BTS_DEVICE $BTS_TYPE $BTS_BAUD $BTS_ADDRESS";;
+  *) failed "Bluetooth QSoC firmware download failed" $exit_code_hci_qcomm_init;;
 esac
 
 # init does SIGTERM on ctl.stop for service
@@ -263,8 +240,7 @@ trap "kill_hciattach" TERM INT
 
 case $TRANSPORT in
     "smd")
-        #echo 1 > /sys/module/hci_smd/parameters/hcismd_set
-        setprop bluetooth.status on
+        echo 1 > /sys/module/hci_smd/parameters/hcismd_set
      ;;
      *)
         logi "start hciattach"
